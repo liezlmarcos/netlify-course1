c0f011aec5eaecaa71a17b875e6dcce55597e137
liezlmarcos <135579545+liezlmarcos@users.noreply.github.com>
2023-06-24 11:25:06 +0400

    testing deployment

---
+++ assets/data.js
@@ -0,0 +1,40 @@
+const items = [
+  {
+    id: 'recmg2a1ctaEJNZhu',
+    name: 'utopia sofa',
+    image: {
+      url:
+        'https://cdn.shopify.com/s/files/1/0474/7087/0679/files/H1_MOBILE_EN_2.jpg?v=1686950864',
+    },
+    price: 39.95,
+  },
+  {
+    id: 'recvKMNR3YFw0bEt3',
+    name: 'entertainment center',
+    image: {
+      url:
+        'https://cdn.shopify.com/s/files/1/0474/7087/0679/files/H2_EN.jpg?v=1686652645',
+    },
+    price: 29.98,
+  },
+  {
+    id: 'recxaXFy5IW539sgM',
+    name: 'albany sectional',
+    image: {
+      url:
+        'https://cdn.shopify.com/s/files/1/0474/7087/0679/files/H1_EN_49f4ee41-2663-492b-8229-4bf13b2ef563.jpg?v=1686216584',
+    },
+    price: 10.99,
+  },
+  {
+    id: 'recyqtRglGNGtO4Q5',
+    name: 'leather sofa',
+    image: {
+      url:
+        'https://cdn.shopify.com/s/files/1/0474/7087/0679/files/H1_M_UAE.jpg?v=1682917657',
+    },
+    price: 9.99,
+  },
+]
+
+module.exports = items
